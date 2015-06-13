0395b7f725a2def8a46502eeead08cd6dd4f919f
astex <0astex@gmail.com>
2015-06-13 11:11:57 -0400

    Add splash screen.

--- front/views.js
+++ front/views.js
@@ -2,8 +2,9 @@ define(
   [
     'jquery', 'underscore', 'backbone',
     'text!templates/header.utpl',
+    'text!templates/splash.utpl',
     'underscore.crunch'
-  ], function($, _, B, t_header) {
+  ], function($, _, B, t_header, t_splash) {
     var V = {};
 
     V.Main = B.View.extend({
@@ -11,7 +12,10 @@ define(
 
       initialize: function() {
         var v = this;
-        v.$el.append((new V.Header()).on('ready', function() { v.trigger('read'); }).el);
+        var ready = _.after(1, function() { v.trigger('ready'); });
+
+        //v.$el.append((new V.Header()).on('ready', ready).el);
+        v.$el.append((new V.Splash()).on('ready', ready).el);
       }
     });
 
@@ -34,6 +38,11 @@ define(
     });
 
     V.Header = V.Base.extend({ el: '<header></header>', t: _.template(t_header) });
+
+    V.Splash = V.Base.extend({
+      el: '<section class="body splash"></section>',
+      t: _.template(t_splash)
+    });
 
     return V;
   }
