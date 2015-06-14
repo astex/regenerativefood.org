15ad8707799b60daec89356c7f4caf4c9e628ecf
astex <0astex@gmail.com>
2015-06-13 21:15:43 -0400

    Add slug before saving in entry controller post.

--- back/app/controllers/entry.py
+++ back/app/controllers/entry.py
@@ -1,6 +1,20 @@
+from app.lib.database import db
 from app.controllers.base import RestController
 from app.models.entry import Entry
 
 
 class EntryController(RestController):
     Model = Entry
+
+    def post(self, data):
+        data = data.copy()
+
+        slug = data.get('slug')
+        title = data.get('title')
+        if not slug:
+            slug = title.lower().replace(' ', '-')
+        if db.session.query(Entry).filter(Entry.slug == slug).first():
+            slug = slug + uuid.uuid4()
+        data['slug'] = slug
+
+        super(EntryController, self).post(data)
