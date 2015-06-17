8b4c483c13d45aa70ec63b83778fc08534cde8ea
astex <0astex@gmail.com>
2015-06-17 15:10:00 -0400

    Add custom commit method to database lib.

--- back/app/lib/database.py
+++ back/app/lib/database.py
@@ -2,3 +2,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+def commit():
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        raise e
